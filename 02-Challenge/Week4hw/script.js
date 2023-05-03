18918475f47fdb10ea5f4fb83c9ae1416de95d0f
ToriKnix <toriknixmusic@gmail.com>
2023-05-02 21:01:36 -0600

    Fully updated

--- 02-Challenge/Week4hw/script.js
+++ 02-Challenge/Week4hw/script.js
@@ -1,12 +1,13 @@
 var duration = 300;
 var timeDisplay = document.getElementById('timer');
 var timer = setInterval(updateTimer, 300000);
+console.log("You have 5 minutes")
 
 function updateTimer() {
     var minutes = Math.floor(duration / 60);
     var seconds = duration % 60;
     var displayString = '';
-    if (minutes < 10) {
+    if (minutes < 1) {
         displayString += '0';
     }
     displayString += minutes + ':';
