0484c8dca91ed4319a0a01d5dbfbc06e635619ab
ToriKnix <toriknixmusic@gmail.com>
2023-05-18 17:48:14 -0600

    Changed a some minor things. Still trying to get the timer to work.

--- 02-Challenge/Week4hw/script.js
+++ 02-Challenge/Week4hw/script.js
@@ -1,13 +1,13 @@
 var duration = 300;
 var timeDisplay = document.getElementById('timer');
-var timer = setInterval(updateTimer, 300000);
+var timer = setInterval(updateTimer, 1000);
 console.log("You have 5 minutes")
 
 function updateTimer() {
     var minutes = Math.floor(duration / 60);
     var seconds = duration % 60;
     var displayString = '';
-    if (minutes < 1) {
+    if (minutes < 10) {
         displayString += '0';
     }
     displayString += minutes + ':';
@@ -15,7 +15,7 @@ function updateTimer() {
         displayString += '0';
     }
     displayString += seconds;
-    timeDisplay.textContent = displayString;
+    timeDisplay.textContent = "Time Remaining: " + displayString;
     duration--;
     if (duration < 0) {
         clearInterval(timer);
